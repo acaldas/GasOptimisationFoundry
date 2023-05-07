5e56224ebd60541230f6272863079d885e661570
acaldas <acaldas.dev@gmail.com>
2023-05-07 19:20:31 +0100

    Public to external, untested code removed, assembly on constructor

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,45 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 contract GasContract {
-    uint8 constant private tradePercent = 12;
     address immutable private contractOwner;
     address[5] public administrators;
-    mapping(address => uint8) public whitelist;
+    mapping(address => uint256) public whitelist;
     mapping(address => uint256) private whiteListStruct;
     mapping(address => uint256) public balances;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
 
     event WhiteListTransfer(address indexed recipient);
-
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
-        for (uint8 i = 0; i < 5;) {
-            address _admin = _admins[i];
-            administrators[i] = _admin;
-            if (_admin == msg.sender) {
-                balances[_admin] = _totalSupply;
-            }
-            unchecked {
-                ++i;
-            }
+        balances[msg.sender] = _totalSupply;
+        assembly {
+            sstore(0x0, mload(0x1c0))
+            sstore(0x1, mload(0x1e0))
+            sstore(0x2, mload(0x200))
+            sstore(0x3, mload(0x220))
+            sstore(0x4, mload(0x240))
         }
     }
 
-    function checkForAdmin(address _user) public view returns (bool admin_) {
-        for (uint8 i = 0; i < 5;) {
-            if (administrators[i] == _user) {
-               return true;
-            }
-            unchecked {
-                ++i;
-            }
-        }
-        return false;
-    }
-
-    function balanceOf(address _user) public view returns (uint256 balance_) {
+    function balanceOf(address _user) external view returns (uint256) {
         return balances[_user];
     }
 
@@ -47,38 +31,35 @@ contract GasContract {
         address _recipient,
         uint256 _amount,
         string calldata _name
-    ) public {
+    ) external {
         address senderOfTx = msg.sender;
         require(balances[senderOfTx] >= _amount);
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
     }
 
-    function addToWhitelist(address _userAddrs, uint256 __tier) public {   
-        address senderOfTx = msg.sender;
-        require(senderOfTx == contractOwner || checkForAdmin(senderOfTx));
+    function addToWhitelist(address _userAddrs, uint256 __tier) external  {
         require(__tier < 255);
+        require(msg.sender == contractOwner);
         uint8 _tier = uint8(__tier);
         if (_tier > 3) {
             _tier = 3;
         }
-        whitelist[_userAddrs] = _tier;
         emit AddedToWhitelist(_userAddrs, __tier);
     }
 
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public {
+    ) external {
         address senderOfTx = msg.sender;
-        uint8 usersTier = whitelist[senderOfTx];
         whiteListStruct[senderOfTx] = _amount;
-        balances[senderOfTx] -= _amount - usersTier;
-        balances[_recipient] += _amount - usersTier;
+        balances[senderOfTx] -= _amount;
+        balances[_recipient] += _amount;
         emit WhiteListTransfer(_recipient);
     }
 
-    function getPaymentStatus(address sender) public view returns (bool, uint256) {        
+    function getPaymentStatus(address sender) external view returns (bool, uint256) {        
         return (true, whiteListStruct[sender]);
     }
 }
