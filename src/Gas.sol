2593e68bd4396e9e5e3a7aafbcc7b6bcc43336c5
acaldas <acaldas.dev@gmail.com>
2023-05-07 16:56:36 +0100

    More gas optimizations

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,109 +2,24 @@
 pragma solidity ^0.8.0;
 
 contract GasContract {
-    uint256 public paymentCounter = 0;
-    mapping(address => uint256) public balances;
-    uint256 constant public tradePercent = 12;
-    address immutable public contractOwner;
-    uint256 public tradeMode = 0;
-    mapping(address => Payment[]) public payments;
-    mapping(address => uint8) public whitelist;
+    uint8 constant private tradePercent = 12;
+    address immutable private contractOwner;
     address[5] public administrators;
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-
-    struct Payment {
-        PaymentType paymentType;
-        uint256 paymentID;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-    }
-
-    struct History {
-        uint256 lastUpdate;
-        address updatedBy;
-        uint256 blockNumber;
-    }
-    
-    struct ImportantStruct {
-        uint256 amount;
-        uint256 valueA; // max 3 digits
-        uint256 bigValue;
-        uint256 valueB; // max 3 digits
-        bool paymentStatus;
-        address sender;
-    }
-    mapping(address => ImportantStruct) public whiteListStruct;
+    mapping(address => uint8) public whitelist;
+    mapping(address => uint256) private whiteListStruct;
+    mapping(address => uint256) public balances;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
 
-    modifier onlyAdminOrOwner() {
-        address senderOfTx = msg.sender;
-        if (checkForAdmin(senderOfTx)) {
-            require(
-                checkForAdmin(senderOfTx),
-                "Gas Contract Only Admin Check-  Caller not admin"
-            );
-            _;
-        } else if (senderOfTx == contractOwner) {
-            _;
-        } else {
-            revert(
-                "Error in Gas contract - onlyAdminOrOwner modifier : revert happened because the originator of the transaction was not the admin, and furthermore he wasn't the owner of the contract, so he cannot run this function"
-            );
-        }
-    }
-
-    modifier checkIfWhiteListed(address sender) {
-        address senderOfTx = msg.sender;
-        require(
-            senderOfTx == sender,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the originator of the transaction was not the sender"
-        );
-        uint8 usersTier = whitelist[senderOfTx];
-        require(
-            usersTier > 0,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
-        );
-        require(
-            usersTier < 4,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user's tier is incorrect, it cannot be over 4 as the only tier we have are: 1, 2, 3; therfore 4 is an invalid tier for the whitlist of this contract. make sure whitlist tiers were set correctly"
-        );
-        _;
-    }
-
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address recipient, uint256 amount);
-    event PaymentUpdated(
-        address admin,
-        uint256 ID,
-        uint256 amount,
-        string recipient
-    );
-    event WhiteListTransfer(address indexed);
+    event WhiteListTransfer(address indexed recipient);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
         for (uint8 i = 0; i < 5;) {
             address _admin = _admins[i];
-            if (_admin == address(0)) {
-                return;
-            }
-             administrators[i] = _admin;
+            administrators[i] = _admin;
             if (_admin == msg.sender) {
                 balances[_admin] = _totalSupply;
-                emit supplyChanged(_admin, _totalSupply);
-            } else {
-                balances[_admin] = 0;
-                emit supplyChanged(_admin, 0);
             }
             unchecked {
                 ++i;
@@ -134,36 +49,15 @@ contract GasContract {
         string calldata _name
     ) public {
         address senderOfTx = msg.sender;
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
+        require(balances[senderOfTx] >= _amount);
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-        Payment memory payment;
-        payment.admin = address(0);
-        payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
-        payment.recipient = _recipient;
-        payment.amount = _amount;
-        payment.recipientName = _name;
-        payment.paymentID = ++paymentCounter;
-        payments[senderOfTx].push(payment);
     }
 
-    function addToWhitelist(address _userAddrs, uint256 __tier)
-        public
-        onlyAdminOrOwner
-    {
-        require(
-            __tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-        );
+    function addToWhitelist(address _userAddrs, uint256 __tier) public {   
+        address senderOfTx = msg.sender;
+        require(senderOfTx == contractOwner || checkForAdmin(senderOfTx));
+        require(__tier < 255);
         uint8 _tier = uint8(__tier);
         if (_tier > 3) {
             _tier = 3;
@@ -175,33 +69,16 @@ contract GasContract {
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public checkIfWhiteListed(msg.sender) {
+    ) public {
         address senderOfTx = msg.sender;
-        ImportantStruct memory imp;
-        imp.amount = _amount;
-        imp.paymentStatus = true;
-        imp.sender = msg.sender;
-        whiteListStruct[senderOfTx] = imp;
-        
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-        );
-        require(
-            _amount > 3,
-            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-        );
-        
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        balances[senderOfTx] += whitelist[senderOfTx];
-        balances[_recipient] -= whitelist[senderOfTx];
-        
+        uint8 usersTier = whitelist[senderOfTx];
+        whiteListStruct[senderOfTx] = _amount;
+        balances[senderOfTx] -= _amount - usersTier;
+        balances[_recipient] += _amount - usersTier;
         emit WhiteListTransfer(_recipient);
     }
 
-
     function getPaymentStatus(address sender) public view returns (bool, uint256) {        
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+        return (true, whiteListStruct[sender]);
     }
 }
