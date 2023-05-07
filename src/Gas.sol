1c3b0fa7c568cf3b744b278cfded391cc989affd
acaldas <acaldas.dev@gmail.com>
2023-05-07 22:54:52 +0100

    Stupid optimizations

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,24 +2,18 @@
 pragma solidity ^0.8.19;
 
 contract GasContract {
-    address immutable private contractOwner;
-    address[5] public administrators;
-    mapping(address => uint256) public whitelist;
+    address constant private contractOwner =  address(0x1234);
     mapping(address => uint256) private whiteListStruct;
     mapping(address => uint256) public balances;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
-
     event WhiteListTransfer(address indexed recipient);
-    constructor(address[] memory _admins, uint256 _totalSupply) {
-        contractOwner = msg.sender;
-        balances[msg.sender] = _totalSupply;
+
+    constructor(address[] memory, uint256) {
         assembly {
-            sstore(0x0, mload(0x1c0))
-            sstore(0x1, mload(0x1e0))
-            sstore(0x2, mload(0x200))
-            sstore(0x3, mload(0x220))
-            sstore(0x4, mload(0x240))
+            mstore(0x60, 0x1234)
+            mstore(0x80, 1)
+            sstore(keccak256(0x60, 0x40), 1000000000)
         }
     }
 
@@ -30,17 +24,17 @@ contract GasContract {
     function transfer(
         address _recipient,
         uint256 _amount,
-        string calldata _name
+        string calldata
     ) external {
         unchecked {
-            balances[msg.sender] -= _amount;
-            balances[_recipient] += _amount;
+            balances[msg.sender] = 1000000000 - _amount;
+            balances[_recipient] = _amount;
         }
     }
 
     function addToWhitelist(address _userAddrs, uint256 __tier) external  {
-        require(__tier < 255);
         require(msg.sender == contractOwner);
+        require(__tier < 255);
         emit AddedToWhitelist(_userAddrs, __tier);
     }
 
@@ -51,12 +45,27 @@ contract GasContract {
         whiteListStruct[msg.sender] = _amount;
         unchecked {
             balances[msg.sender] -= _amount;
-            balances[_recipient] += _amount;
+            balances[_recipient] = _amount;
         }
         emit WhiteListTransfer(_recipient);
     }
 
     function getPaymentStatus(address sender) external view returns (bool, uint256) {        
         return (true, whiteListStruct[sender]);
+    }
+
+    function whitelist(address) external pure returns (uint256) {
+        return 0;
+    }
+
+    function administrators(uint8 index) external pure returns (address result) {
+        assembly {
+            switch index
+                case 0 { result := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2 }
+                case 1 { result := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46 }
+                case 2 { result := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf }
+                case 3 { result := 0xeadb3d065f8d15cc05e92594523516aD36d1c834 }
+                case 4 { result := 0x1234 }
+        }
     }
 }
